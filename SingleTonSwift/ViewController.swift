1c6e595b94d96816ef8156aedd71e84711955ac7
RanjithBaddam0563 <60254897+RanjithBaddam0563@users.noreply.github.com>
2021-08-05 15:29:12 +0530

    Initial Commit

---
+++ SingleTonSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingleTonSwift
+//
+//  Created by MicroExcel on 8/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
